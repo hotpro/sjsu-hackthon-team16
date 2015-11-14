2f6267fdb1447c3613511ae5797d297a76a1d082
Yunlong <vincent881229@gmail.com>
2015-11-13 21:34:41 -0800

    add notification

---
+++ app/src/main/java/edu/sjsu/hackthon/sjsuteam16/NotificationUtils.java
@@ -0,0 +1,10 @@
+package edu.sjsu.hackthon.sjsuteam16;
+
+/**
+ * Created by yunlongxu on 11/13/15.
+ */
+public class NotificationUtils {
+    public static void showNotification() {
+        
+    }
+}
