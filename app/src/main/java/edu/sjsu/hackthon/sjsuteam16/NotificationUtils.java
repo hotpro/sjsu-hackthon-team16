322546d4d106a2876fdda1010478f187c6630f46
Yunlong <vincent881229@gmail.com>
2015-11-13 22:02:32 -0800

    implement notification

--- app/src/main/java/edu/sjsu/hackthon/sjsuteam16/NotificationUtils.java
+++ app/src/main/java/edu/sjsu/hackthon/sjsuteam16/NotificationUtils.java
@@ -1,10 +1,44 @@
 package edu.sjsu.hackthon.sjsuteam16;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+
 /**
  * Created by yunlongxu on 11/13/15.
  */
 public class NotificationUtils {
-    public static void showNotification() {
-        
+    public static void showNotification(Context context) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.ic_drawer)
+                .setContentTitle("GAMESTOP Notification")
+                .setContentText("Game Promotion");
+
+        // Creates an explicit intent for an Activity in app
+        Intent resultIntent = new Intent(context, MainActivity.class);
+
+        // The stack builder object will contain an aritificial back stack for the
+        // started Activity
+        // This ensures that navigating backward from the Activity leads out of
+        // application to the Home Screen
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+        // Adds the back stack for the Intent
+        stackBuilder.addParentStack(MainActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+//        NotificationManager mNotificationManager =
+//                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+//        mNotificationManager.notify(mId, mBuilder.build());
     }
 }
